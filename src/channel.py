b4f8a1ffbb37866f7bdd25838c3149b538200a2c
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2023-11-12 14:47:23 +0300

    Done

--- src/channel.py
+++ src/channel.py
@@ -20,10 +20,9 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        printj(channel)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        printj(channel)
