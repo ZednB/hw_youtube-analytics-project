9096b597a0fab87b2ae79d6b13b9b014ff72fec4
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2023-11-12 21:57:05 +0300

    Done

--- src/channel.py
+++ src/channel.py
@@ -1,15 +1,7 @@
 import json
 import os
 from googleapiclient.discovery import build
-
-
-api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
-
-
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+from helper.youtube_api_manual import youtube
 
 
 class Channel:
@@ -20,9 +12,12 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        printj(channel)
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+
+x = Channel('UCwHL6WHUarjGfUM_586me8w')
+x.print_info()
