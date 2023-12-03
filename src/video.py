68ed586a785dfae5c7c4ea32cb984c879e8f7845
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2023-12-03 20:00:29 +0300

    Done

--- src/video.py
+++ src/video.py
@@ -6,31 +6,34 @@ class Video:
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, video_id: str) -> None:
-        self.video_id = video_id
-        self.video_name = self.get_video_name()[0]
-        self.link_to_video = 'https://www.youtube.com/' + self.video_id
-        self.number_views = self.get_video_name()[1]
-        self.number_of_likes = self.get_video_name()[2]
+    def __init__(self, id_video):
+        self.id_video = id_video
+        self.title = self.get_video_name()[0]
+        self.video_url = 'https://www.youtube.com/watch?v=' + self.id_video
+        self.view_count = self.get_video_name()[1]
+        self.like_count = self.get_video_name()[2]
 
     def get_video_name(self):
-        video_info = []
-        video_id = self.video_id
-        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                    id=video_id
-                                                    ).execute()
-        video_title: str = video_response['items'][0]['snippet']['title']
-        video_info.append(video_title)
-        view_count: int = video_response['items'][0]['statistics']['viewCount']
-        video_info.append(view_count)
-        like_count: int = video_response['items'][0]['statistics']['likeCount']
-        video_info.append(like_count)
-        comment_count: int = video_response['items'][0]['statistics']['commentCount']
-        video_info.append(comment_count)
-        return video_info
+        list_value = []
+        video_id = self.id_video
+        try:
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                        id=video_id
+                                                        ).execute()
+            video_title: str = video_response['items'][0]['snippet']['title']
+            list_value.append(video_title)
+            view_count: int = video_response['items'][0]['statistics']['viewCount']
+            list_value.append(view_count)
+            like_count: int = video_response['items'][0]['statistics']['likeCount']
+            list_value.append(like_count)
+        except IndexError:
+            list_value.append(None)
+            list_value.append(None)
+            list_value.append(None)
+        return list_value
 
     def __str__(self):
-        return str(self.video_name)
+        return str(self.title)
 
 
 class PLVideo(Video):
@@ -40,4 +43,4 @@ class PLVideo(Video):
         self.playlist_id = playlist_id
 
     def __str__(self):
-        return str(self.video_name)
+        return str(self.title)
